60890b9f0e0c51f84f2d8c510420947ef1dba426
TupickiyV <vladislav.tupickiy@gmail.com>
2015-12-23 14:19:28 +0200

    add fields and validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,9 @@ class Order < ActiveRecord::Base
   has_many :products, class_name: 'OrderProduct'
 
   accepts_nested_attributes_for :products
-
+  validates :email, presence: true, format: /@/
+  validates :phone, presence: true, format: { with: /\+\d{12}/, message: 'Example: +380XXXXXXXXX' }
+  validates :address, presence: true
   before_save :cal_total
 
   private
